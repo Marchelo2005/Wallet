89d66c5836cff05766b0f5fdc57ec5a261a7a708
Marchelo2005 <marcelonacimba2005@gmail.com>
2024-06-24 15:58:59 -0500

    first commit

---
+++ src/main/java/com/mycompany/wallet/Wallet.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.wallet;
+
+/**
+ *
+ * @author User
+ */
+public class Wallet {
+
+    public static void main(String[] args) {
+      Home object = new Home();
+          object.setVisible(true);
+      
+    }
+}
